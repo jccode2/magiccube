51b11b5fe549c241d778299b1a4bfc4c83a12079
magicvoid <magicvoid@me.com>
2013-06-02 01:51:47 +0800

    调整数据库初始化脚本使之适应新的数据库结构
    用之前的初始化会报错

--- src/main/resources/sql/dbdesign.sql
+++ src/main/resources/sql/dbdesign.sql
@@ -297,11 +297,11 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
------------------------------
---orders 表增加2个字段
-------------------------------
---实际应付的价钱
+-- ---------------------------
+-- orders 表增加2个字段
+-- ----------------------------
+-- 实际应付的价钱
 alter table orders add column actually_price double;
---优惠编码
+-- 优惠编码
 alter table orders add column discount_code varchar(3);
 
