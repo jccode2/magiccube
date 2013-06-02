c52fd404f8ba8fa47a229467a8ddf99d5bfc7b38
jccode2 <jcmagicvoid@gmail.com>
2013-06-03 00:42:12 +0800

    fixed bug: 己处理页面不显示,搜索失效

--- src/main/java/com/magiccube/food/service/FoodService.java
+++ src/main/java/com/magiccube/food/service/FoodService.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 import com.magiccube.core.base.service.BaseService;
 import com.magiccube.core.util.tools.CodeGenerator;
