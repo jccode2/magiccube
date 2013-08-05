78c889ec1e0ff63f193c4a6f4fb285bd032bb02c
jccode2 <jcmagicvoid@gmail.com>
2013-08-05 23:42:39 +0800

    add bulletin 10%

--- src/main/resources/sql/update/02_bulletin_table.sql
+++ src/main/resources/sql/update/02_bulletin_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `1000funs`.`bulletin` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `content` VARCHAR(45) NULL ,
+  `sort` INT NULL DEFAULT 0 ,
   `enable` TINYINT(1) NULL DEFAULT true ,
   `deleted` TINYINT(1) NULL DEFAULT false ,
   PRIMARY KEY (`id`) )
